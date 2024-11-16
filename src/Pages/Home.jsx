2224044d3b40a47d19ced5f1adfa55e892e6c4d7
samiuzzaman <chowdhurysamiuzzaman@gmail.com>
2024-11-16 21:40:55 +0600

    Banner Section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Helmet } from "react-helmet-async";
+import Banner from "../components/HomeLayout/Banner";
 
 const Home = () => {
   return (
@@ -7,8 +8,8 @@ const Home = () => {
         <title>Home | React App</title>
         <link rel="canonical" href="/" />
       </Helmet>
-      <div className="w-11/12 mx-auto bg-[#036C5E]">
-        <h2 className="text-red-500">This is Home Page</h2>
+      <div className="w-11/12 mx-auto ">
+        <Banner></Banner>
       </div>
     </div>
   );
---
+++ src/components/HomeLayout/Banner.jsx
@@ -0,0 +1,24 @@
+import { Button } from "keep-react";
+import BannerImage from "../../assets/banner.png";
+
+const Banner = () => {
+  return (
+    <div className="flex flex-col lg:flex-row justify-between items-center">
+      <div className="w-full lg:w-1/2 mt-8 text-center lg:text-left">
+        <h1 className="text-[#1d26a3] text-4xl md:text-5xl lg:text-7xl font-bold">
+          We care about your smill
+        </h1>
+        <p className="text-gray-100 my-6">
+          We are a team of dentists, hygienists, and receptionists who work
+          together to ensure you receive the best treatment you require.
+        </p>
+        <Button variant="default">Book Now</Button>
+      </div>
+      <div>
+        <img src={BannerImage} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
